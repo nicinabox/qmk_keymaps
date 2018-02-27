97bee95574264cf7e18146ca6f9a4924a9444a60
Nic Aitch <nic@nicinabox.com>
2018-02-27 02:04:09 -0600

    Rename bootloader to programmer, tweak rules.mk

--- rules.mk
+++ rules.mk
@@ -2,11 +2,6 @@ NAME = $(shell whoami)
 QMK_DIR = ../qmk_firmware
 KEYBOARDS_PATH = $(QMK_DIR)/keyboards
 ROOT_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-QMK_KEYMAPS_VERSION = 1.0.0
-
-.PHONY: version
-version:
-	@echo $(QMK_KEYMAPS_VERSION)
 
 .PHONY: cp/handwired/%
 cp/handwired/%:
@@ -19,8 +14,7 @@ build\:handwired/%: cp/handwired/%
 
 .PHONY: flash\:handwired/%
 flash\:handwired/%: cp/handwired/%
-	@FLASHER=$(shell ruby $(ROOT_DIR)/bootloader.rb $(KEYBOARDS_PATH)/handwired/$*); \
-	cd $(QMK_DIR) && make handwired/$*:$$FLASHER
+	@cd $(QMK_DIR) && make handwired/$*:$(shell ruby $(ROOT_DIR)/programmer.rb $(KEYBOARDS_PATH)/handwired/$*)
 
 .PHONY: cp/%
 cp/%:
@@ -33,5 +27,4 @@ build\:%: cp/%
 
 .PHONY: flash\:%
 flash\:%: cp/%
-	@FLASHER=$(shell ruby $(ROOT_DIR)/bootloader.rb $(KEYBOARDS_PATH)/$*); \
-	cd $(QMK_DIR) && make $*:$(NAME):$$FLASHER
+	@cd $(QMK_DIR) && make $*:$(NAME):$(shell ruby $(ROOT_DIR)/programmer.rb $(KEYBOARDS_PATH)/$*)
